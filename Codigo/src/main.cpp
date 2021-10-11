e86bf1326f78569c4106ff1a223ca49bc4c7ef10
gabrielnitz <gabriel.lezannitz@gmail.com>
2021-10-11 18:32:52 -0300

    upload

--- Codigo/src/main.cpp
+++ Codigo/src/main.cpp
@@ -1,42 +1,31 @@
 #include <Arduino.h> // Biblioteca do FreeRTOS para o Arduino
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h> // Biblioteca para utilização das tasks
+// #include <freertos/FreeRTOS.h>
+// #include <freertos/task.h> // Biblioteca para utilização das tasks
 // #include <math.h> // Biblioteca para funções matemáticas
 
 // Definir os pinos dos leds
-#define led_amarelo   4
-#define led_vermelho  6
-#define led_verde     8
- 
- void led1Task(void *arg);
+#define temperatura   4
+#define umidade       2
 
-//Tasks
-TaskHandle_t    led1TaskH;
-  
-void setup() {
-    // Inicializa Serial
-    Serial.begin(9600);
-    
-    // Define os leds como saída
-    pinMode(led_verde, OUTPUT);
-    pinMode(led_amarelo, OUTPUT);
-    pinMode(led_vermelho, OUTPUT);
-     
-    // Cria tarefa led1Task
-    xTaskCreate(led1Task,     // Função
-                "led1Task",   // Nome
-                128,          // Pilha
-                NULL,         // Parâmetro
-                1,            // Prioridade
-                &led1TaskH);  // Identificador
-}
- 
-void loop() {
-    // Nada é executado aqui, as tarefas estão nas funções abaixo
-}
- 
-  // Led1Task
-  // Pisca led verde com frequência de 1Hz
+int x, y;
 
-// alo alo alo
+void setup(){
+  Serial.begin(9600);
+
+  pinMode(temperatura,INPUT);
+  pinMode(umidade,INPUT);
+
+
+}
+
+void loop(){
+
+  x = analogRead(temperatura);
+  y = analogRead(umidade);
+
+  Serial.println(temperatura);
+  Serial.println(umidade);
+
+  delay(200);
+}
  
