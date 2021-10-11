073d65dc5678078728618bd824424db59cb535dc
gabrielnitz <gabriel.lezannitz@gmail.com>
2021-10-11 17:14:14 -0300

    novas

--- Codigo/src/nitz.cpp
+++ Codigo/src/nitz.cpp
@@ -1,5 +1,15 @@
-#include <Arduino.h>
+#include <Arduino.h> // Biblioteca do FreeRTOS para o Arduino
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h> 
+#define led_amarelo   4
+#define led_vermelho  6
+#define led_verde     8
 
-void teste2(void){
-  Serial.println("teste2");
+void led1Task(void *arg) {
+    while(1) { // Loop infinito
+        digitalWrite(led_verde, HIGH);    // Liga o led
+        vTaskDelay(pdMS_TO_TICKS(500));   // Espera 0.5s
+        digitalWrite(led_verde, LOW);     // Desliga o led
+        vTaskDelay(pdMS_TO_TICKS(500));   // Espera 0.5s
+    }
 }
