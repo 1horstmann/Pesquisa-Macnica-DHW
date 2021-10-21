b55d45f95d65e3878d8754500932abc276e29e8b
Guilherme Tsubahara Horstmann <guilhermehorstmann1@gmail.com>
2021-10-21 16:45:59 -0300

    Criei a função que le a temp. sem rtos

--- Codigo/src/main.cpp
+++ Codigo/src/main.cpp
@@ -1,21 +1,18 @@
-#include <Arduino.h> // Biblioteca do FreeRTOS para o Arduino
-#include <freertos/FreeRTOS.h>
-#include <freertos/timers.h>
-// #include <freertos/task.h> // Biblioteca para utilização das tasks
-// #include <math.h> // Biblioteca para funções matemáticas
+#include <Arduino.h> 
 
 /* Definindo os pinos */
 #define temp   4
 #define umi    2
+#define led_temp 18 
 
 /* Definindo constantes */
 #define periodo_leitura 3000
 
-/* Criando as tarefas */
-TaskHandle_t t_readH;
 
 /* Protótipos de funções */
-void t_read(void *arg);
+void t_read(void);
+
+
 
 void setup(){
 
@@ -25,25 +22,27 @@ void setup(){
   /* Definindo os IO */
   pinMode(temp,INPUT);
   pinMode(umi,INPUT);
-
-  /* Criando as Tarefas */
-  xTaskCreate(t_read, "t_read", 128, NULL, 1, &t_readH);
+  pinMode(led_temp, OUTPUT);
 
 }
 
-void loop(){}
+void loop(){
+  
+  
+  t_read();
 
-/* Função de le a temperatura */
-void t_read(void *arg){
-  while(1){
+}
+
+/* Função que le a temperatura */
+void t_read(void){
     float l_temp = analogRead(temp)*3.3/4095.0; // Conversão bits para tensão
     float tempC = l_temp*100.0; // Conversão tensão para temperatura
 
+    Serial.println(analogRead(temp));
     Serial.print("Temperatura: ");
     Serial.print((unsigned)tempC);
     Serial.println(" °C");
-
+    digitalWrite(led_temp, !digitalRead(led_temp));
     vTaskDelay(pdMS_TO_TICKS(periodo_leitura));
-  }
 }
- 
+
