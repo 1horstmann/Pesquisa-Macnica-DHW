76588126ccca504d8375604be5990f743c10875c
gabrielnitz <gabriel.lezannitz@gmail.com>
2021-10-26 18:57:07 -0300

    wifi 26

--- Codigo/src/main.cpp
+++ Codigo/src/main.cpp
@@ -24,11 +24,16 @@ WiFiServer server(80);
 
 void setup(){
 
+  
+
   /* Comunicação Serial */
   Serial.begin(115200);
 
   // Inicialização da rede wifi
-  //WiFi.begin(nome, senha);
+  Serial.println();
+  Serial.print("Conectando-se a ");
+  Serial.println(nome);
+  WiFi.begin(nome, senha);
 
   // "WiFi.status()" retorna uma constate informando se houve problema de conexão ou não
   /*
@@ -41,25 +46,72 @@ void setup(){
         WL_IDLE_STATUS: quando está ou em processo de tentativas de conexão.
   */
 
-  /*while(WiFi.status() != WL_CONNECTED) 
+  while(WiFi.status() != WL_CONNECTED) 
   {
     delay(500);
     Serial.println("Conectando wifi..");
   }
-  Serial.println("Conectado a rede wifi");*/
-  WiFi.mode(WIFI_MODE_STA);
- Serial.println(WiFi.macAddress());
+  Serial.println("Conectado a rede wifi");
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+
+
+  //WiFi.mode(WIFI_MODE_STA); // Essa função ajusta o modo de operação do chip WiFi da placa
+  //Serial.println(WiFi.macAddress()); // Rretorna o endereço MAC de um dispositivo de rede (em hexadecimal) 
 
   /* Definindo os IO */
   pinMode(temp,INPUT);
   pinMode(umi,INPUT);
   pinMode(led_temp, OUTPUT);
+  pinMode(2,OUTPUT);
 
-  //server.begin();
+  
 }
 
 void loop(){
-  
+  WiFiClient cliente = server.available();
+  if (cliente) {
+    Serial.println("Novo cliente");
+    String currentLine = "";
+    while (cliente.connected()) {
+      if (cliente.available()) {
+        char c = cliente.read();
+        Serial.write(c);
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            cliente.println("HTTP/1.1 200 OK");
+            cliente.println("Content-type:text/html");
+            cliente.println();
+            cliente.print("Click <a href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
+            cliente.println();
+            cliente.print("Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
+            cliente.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(2, HIGH);
+          Serial.println("CLIOCU EM OUTRO");
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(2, LOW);
+          Serial.println("CLIOCU EM UM");
+        }
+      }
+    }
+
+    //cliente.println("Oi");
+    //cliente.println("Tudo bem?");
+
+    cliente.stop();
+    Serial.println("Client Disconnected.");
+  }
   
   //t_read();
 
