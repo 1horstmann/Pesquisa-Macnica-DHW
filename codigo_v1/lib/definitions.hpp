b615a0d85e7aec5d5c9a00448184b6eb508bc8e0
Guilherme Tsubahara Horstmann <guilhermehorstmann1@gmail.com>
2021-12-09 15:10:28 -0300

    AAA

---
+++ codigo_v1/lib/definitions.hpp
@@ -0,0 +1,15 @@
+#ifndef DEFINITIONS
+#define DEFINITIONS
+
+/* DEFINIÇÕES DOS PINOS */
+#define POTENCIOMETER       3
+#define SENSOR_TEMPERATURE  4
+#define SENSOR_MOISTURE     2
+#define SENSOR_ACCELOMETER  2
+
+
+
+
+
+
+#endif
